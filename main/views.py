79207b97703d1902e1b94b25993a21e17d92df36
Antonio <famegg.contact@gmail.com>
2023-04-14 15:10:29 -0400

    dynamic pages

--- main/views.py
+++ main/views.py
@@ -5,3 +5,7 @@ from .models import Project
 def home(request):
     projects = Project.objects.all().order_by('rank')
     return render(request, 'home.html', {'projects': projects})
+
+def project(request, name):
+    project = Project.objects.get(name=name)
+    return render (request, 'project_page.html', {'project':project})
